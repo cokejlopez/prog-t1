907b179e257588f35eeb83ccecea38253a84b44b
coke <77961614+cokejlopez@users.noreply.github.com>
2026-09-23 14:44:27 +0200

    creado del holamundo

---
+++ holamundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hola mundo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
