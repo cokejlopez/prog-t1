f9d98be6b091028e8b1a1c73976e8401222ac9f9
coke <77961614+cokejlopez@users.noreply.github.com>
2026-09-25 08:23:44 +0200

    24/09/2026

---
+++ unidad01/src/unidad01/Ejemplo2.java
@@ -0,0 +1,14 @@
+package unidad01;
+
+import java.time.LocalTime;
+
+public class Ejemplo2 {
+
+	public static void main(String[] args) {
+		LocalTime horaLocal = LocalTime.now();
+		System.out.printf("la hora actual es " + horaLocal);
+				
+
+	}
+
+}
